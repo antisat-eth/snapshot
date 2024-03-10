1ec26474fb0c8a90480efe1c44dd1437c3ff8410
Sinuhe Coronel <lesmo@lesmo.com.mx>
2024-03-09 22:10:11 -0600

    fix: remove 3 dot menu

--- src/components/SpaceSidebar.vue
+++ src/components/SpaceSidebar.vue
@@ -13,11 +13,6 @@ defineProps<{ space: ExtendedSpace }>();
         <SpaceSidebarNavigation :space="space" class="lg:mt-0" />
         <SpaceSidebarSubspaces :space="space" class="hidden lg:flex" />
         <SpaceSidebarFooter :space="space" class="hidden lg:flex" />
-        <div
-          class="absolute -top-1 right-[16px] md:right-[12px] md:top-[10px] lg:right-[10px]"
-        >
-          <SpaceSidebarMenuThreeDot class="hidden lg:block" />
-        </div>
       </div>
     </BaseBlock>
   </div>
