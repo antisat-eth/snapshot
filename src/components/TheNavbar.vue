67f14f396b3e522b86ee6f6abe031aa7380327f0
Sinuhe Coronel <lesmo@lesmo.com.mx>
2024-03-09 22:02:36 -0600

    chore: update header

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -30,7 +30,7 @@ watch(
             class="hidden items-center sm:block"
             style="font-size: 24px"
           >
-            snapshot
+            dao.antisat.io
           </router-link>
           <span
             v-if="env === 'demo'"
